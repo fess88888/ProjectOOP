12b8a2d51291bb9e6a5fc5842326a27918bbb549
Виталий Якимов <fess.888@mail.ru>
2026-04-17 16:25:43 +0600

    changes in category.py

--- src/category.py
+++ src/category.py
@@ -1,5 +1,9 @@
+from src.product import Product
+
+
 class Category:
     """Класс категорий товаров"""
+
     name: str
     description: str
     products: list
@@ -9,6 +13,17 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_list = [
+            f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products
+        ]
+        return "\n".join(product_list) + "\n"
