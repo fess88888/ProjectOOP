6f76f492e16737b2e1a1e3e81ea3ac78dca6e27c
Виталий Якимов <fess.888@mail.ru>
2026-04-17 19:47:58 +0600

    changes in category.py, product.py and utils.py

--- src/product.py
+++ src/product.py
@@ -1,12 +1,44 @@
+from typing import Any
+
+
 class Product:
-    """Класс характеристик товара"""
+    """Класс для представления характеристик товара"""
+
     name: str
     description: str
-    price: float
     quantity: int
+    product_list: list = []
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+        """Инициализирует новый экземпляр товара.
+        При создании объекта автоматически добавляет его в список product_list."""
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+        Product.product_list.append(self)
+
+    @classmethod
+    def new_product(cls, product: dict) -> Any:
+        """Добавляет новый товар или обновляет существующий с таким же именем."""
+
+        existing_product = None
+        for unit in cls.product_list:
+            if product["name"] == unit.name:
+                existing_product = unit
+                break
+
+        if existing_product:
+            # Обновляем существующий товар
+            existing_product.price = max(existing_product.price, product["price"])
+            existing_product.quantity += product["quantity"]
+            return existing_product
+        else:
+            # Создаём новый товар
+            new_unit = cls(
+                name=product["name"],
+                description=product["description"],
+                price=product["price"],
+                quantity=product["quantity"],
+            )
+            return new_unit
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,20 @@
 import json
 import os
+from typing import Any
 
 from src.product import Product
 from src.category import Category
 
 
-def loads_category_and_product_data_from_json(path: str) -> dict:
-    """ Функция, которая загружает данные по категориям и товарам из файла JSON"""
+def loads_category_and_product_data_from_json(path: str) -> Any:
+    """Функция, которая загружает данные по категориям и товарам из файла JSON"""
     path_to_file = os.path.abspath(path)
-    with open(path_to_file, 'r', encoding="UTF-8") as file:
+    with open(path_to_file, "r", encoding="UTF-8") as file:
         category_and_product_data = json.load(file)
         return category_and_product_data
 
 
-def create_object_from_json(category_and_product_data: dict):
+def create_object_from_json(category_and_product_data: list[dict]) -> list[Category]:
     """Функция, которая конвертирует полученные данные из файла JSON в объекты классов."""
     categories = []
     for category in category_and_product_data:
@@ -25,7 +26,7 @@ def create_object_from_json(category_and_product_data: dict):
     return categories
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     my_data = loads_category_and_product_data_from_json("../data/products.json")
     print(my_data)
     categories_data = create_object_from_json(my_data)
@@ -33,4 +34,3 @@ if __name__ == '__main__':
     print(categories_data[0].name)
     print(categories_data[0].description)
     print(categories_data[0].products)
-
