9a47272708017515a7f8581872ee0e43404e7d53
Виталий Якимов <fess.888@mail.ru>
2026-05-01 14:47:39 +0600

    add class Smartphone and LawnGrass in module product.py

--- src/product.py
+++ src/product.py
@@ -74,3 +74,43 @@ class Product:
     def __add__(self, other) -> float:
         """Получает полную стоимость всех товаров на складе."""
         return (self.price * self.quantity) + (other.price * other.quantity)
+
+
+class Smartphone(Product):
+    """Класс товаров: Смартфоны"""
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        efficiency: float,
+        model: str,
+        memory: int,
+        color: str,
+    ) -> None:
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    """Класс товаров: Трава газонная"""
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        country: str,
+        germination_period: str,
+        color: str,
+    ) -> None:
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
